264bc8e78b2c76794fbd486d2a38180841b0ea13
github username <github email address>
2018-06-01 11:25:13 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+attr_accessor :a,:b,:c
+def initialize(a,b,c)
+  @a=a
+  @b=b
+  @c=c
+end
+
+def kind
+  if @a+@b>@c && @a+@c>@b && @b+@c>@a && @a==@b && @b==@c
+    :equilateral
+  elsif @a+@b>@c && @a+@c>@b && @b+@c>@a &&(@a==@b ||@b==@c || @c==@a)
+    :isosceles
+  elsif @a+@b>@c && @a+@c>@b && @b+@c>@a && @a!=@b && @b!=@c
+    :scalene
+else
+  raise TriangleError
+  end
+end
+class TriangleError < StandardError
+end
+
 end
